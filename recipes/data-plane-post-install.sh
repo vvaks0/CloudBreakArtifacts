1c46fbd2fb45f245f38b5b7e75ae6f7e5244a3f8
vvaks <vvaks@hortonworks.com>
2016-11-17 15:46:54 -0500

    update

--- recipes/data-plane-post-install.sh
+++ recipes/data-plane-post-install.sh
@@ -17,14 +17,7 @@ fi
 git clone https://github.com/vakshorton/CloudBreakArtifacts
 cd CloudBreakArtifacts
 export ROOT_PATH=$(pwd)
-
 echo "*********************************ROOT PATH IS: $ROOT_PATH"
-
-getHiveServerHost () {
-        HIVESERVER_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_SERVER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
-
-        echo $HIVESERVER_HOST
-}
 
 createTransactionHistoryTable () {	
 	HQL="CREATE TABLE IF NOT EXISTS TransactionHistory ( accountNumber String,
@@ -49,8 +42,69 @@ createTransactionHistoryTable () {
 	beeline -u jdbc:hive2://$HIVESERVER_HOST:10000/default -d org.apache.hive.jdbc.HiveDriver -e "$HQL"
 }
 
+
+getNameNodeHost () {
+       	NAMENODE_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HDFS/components/NAMENODE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $NAMENODE_HOST
+}
+
+getHiveServerHost () {
+        HIVESERVER_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_SERVER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $HIVESERVER_HOST
+}
+
+getHiveMetaStoreHost () {
+        HIVE_METASTORE_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_METASTORE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $HIVE_METASTORE_HOST
+}
+
+getKafkaBroker () {
+       	KAFKA_BROKER=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/KAFKA/components/KAFKA_BROKER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $KAFKA_BROKER
+}
+
+getAtlasHost () {
+       	ATLAS_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/ATLAS/components/ATLAS_SERVER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $ATLAS_HOST
+}
+
+export JAVA_HOME=/usr/jdk64
+NAMENODE_HOST=$(getNameNodeHost)
+export NAMENODE_HOST=$NAMENODE_HOST
 HIVESERVER_HOST=$(getHiveServerHost)
 export HIVESERVER_HOST=$HIVESERVER_HOST
+HIVE_METASTORE_HOST=$(getHiveMetaStoreHost)
+export HIVE_METASTORE_HOST=$HIVE_METASTORE_HOST
+HIVE_METASTORE_URI=thrift://$HIVE_METASTORE_HOST:9083
+export HIVE_METASTORE_URI=$HIVE_METASTORE_URI
+ZK_HOST=$AMBARI_HOST
+export ZK_HOST=$ZK_HOST
+KAFKA_BROKER=$(getKafkaBroker)
+export KAFKA_BROKER=$KAFKA_BROKER
+ATLAS_HOST=$(getAtlasHost)
+export ATLAS_HOST=$ATLAS_HOST
+env
+
+echo "export NAMENODE_HOST=$NAMENODE_HOST" >> /etc/bashrc
+echo "export ZK_HOST=$ZK_HOST" >> /etc/bashrc
+echo "export KAFKA_BROKER=$KAFKA_BROKER" >> /etc/bashrc
+echo "export ATLAS_HOST=$ATLAS_HOST" >> /etc/bashrc
+echo "export HIVE_METASTORE_HOST=$HIVE_METASTORE_HOST" >> /etc/bashrc
+echo "export HIVE_METASTORE_URI=$HIVE_METASTORE_URI" >> /etc/bashrc
+
+echo "export NAMENODE_HOST=$NAMENODE_HOST" >> ~/.bash_profile
+echo "export ZK_HOST=$ZK_HOST" >> ~/.bash_profile
+echo "export KAFKA_BROKER=$KAFKA_BROKER" >> ~/.bash_profile
+echo "export ATLAS_HOST=$ATLAS_HOST" >> ~/.bash_profile
+echo "export HIVE_METASTORE_HOST=$HIVE_METASTORE_HOST" >> ~/.bash_profile
+echo "export HIVE_METASTORE_URI=$HIVE_METASTORE_URI" >> ~/.bash_profile
+
+. ~/.bash_profile
 
 #createTransactionHistoryTable
 
