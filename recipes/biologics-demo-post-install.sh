7de93ec2fd5fbc204fa863fd154187e4b3b399a0
vvaks <vvaks@hortonworks.com>
2016-11-23 16:11:47 -0500

    update

--- recipes/biologics-demo-post-install.sh
+++ recipes/biologics-demo-post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec > >(tee -i demoInstall.log)
 exec 2>&1
-echo "*********************************Install Credit Fraud Demo"
+echo "*********************************Install Biologics Demo"
 git clone https://github.com/vakshorton/BiologicsManufacturingDemo
 cd RetailStoreMonitor
 sed -r -i 's;\{\{jdk64_home\}\};/usr/jdk64;' Nifi/config/nifi-env.json
