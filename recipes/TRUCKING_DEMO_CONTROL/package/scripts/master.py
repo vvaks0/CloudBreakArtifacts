6e0f8f7848d8603305598dda64e74d76909d249e
vvaks <vvaks@hortonworks.com>
2017-06-14 10:19:53 -0600

    update

--- recipes/TRUCKING_DEMO_CONTROL/package/scripts/master.py
+++ recipes/TRUCKING_DEMO_CONTROL/package/scripts/master.py
@@ -28,7 +28,7 @@ class DemoControl(Script):
     self.configure(env)
     import params
     Execute('echo Stop Simulation')
-    Execute ('kill -9 $(ps -ef|grep "Data-Loader/data-loader-jar-with-dependencies.jar"| grep -v grep| awk \'{print $2}\') >/dev/null 2>&1') 
+    Execute ('kill -9 `ps -ef|grep "Data-Loader/data-loader-jar-with-dependencies.jar"| grep -v grep| awk \'{print $2}\'` >/dev/null 2>&1') 
     Execute ('rm -f /var/run/TruckSim.pid')
     
   def status(self, env):
