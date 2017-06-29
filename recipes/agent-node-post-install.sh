29c7c0d375adfa654d785c2c10b20ac524f98335
vvaks <vvaks@hortonworks.com>
2017-06-29 13:50:34 -0400

    update

--- recipes/agent-node-post-install.sh
+++ recipes/agent-node-post-install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "/usr/jdk64" ]; then
-	echo "*********************************Install and Enable Oracle JDK 8"
-	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz"
-	tar -vxzf jdk-8u101-linux-x64.tar.gz -C /usr
-	mv /usr/jdk1.8.0_101 /usr/jdk64
-	alternatives --install /usr/bin/java java /usr/jdk64/bin/java 3
-	alternatives --install /usr/bin/javac javac /usr/jdk64/bin/javac 3
-	alternatives --install /usr/bin/jar jar /usr/jdk64/bin/jar 3
-	export JAVA_HOME=/usr/jdk64
-	echo "export JAVA_HOME=/usr/jdk64" >> /etc/bashrc
-fi
-
 waitForServiceToStart () {
        	# Ensure that Service is not in a transitional state
        	SERVICE=$1
@@ -100,7 +88,7 @@ sleep 10
 
 #AMBARI_HOST=$(nslookup $(cat /etc/ambari-agent/conf/ambari-agent.ini| grep hostname= |grep -Po '([0-9.]+)')| grep -Po ' = (.*)'| grep -Po '([0-9a-zA-Z\-_.,?!@#$%^&*]+)'| rev | cut -c 2- | rev)
 
-export JAVA_HOME=/usr/jdk64
+#export JAVA_HOME=/usr/jdk64
 NAMENODE_HOST=$(getNameNodeHost)
 export NAMENODE_HOST=$NAMENODE_HOST
 HIVESERVER_HOST=$(getHiveServerHost)
