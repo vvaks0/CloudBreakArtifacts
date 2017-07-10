a32f890e2ca79d2699ed78099bf161a4a1cc3acb
vvaks <vvaks@hortonworks.com>
2017-07-10 15:21:47 -0600

    update

--- recipes/HISTORIAN/package/scripts/params.py
+++ recipes/HISTORIAN/package/scripts/params.py
@@ -12,7 +12,10 @@ ambari_server_port = '8080'
 cluster_name = str(json.loads(requests.get('http://'+ambari_server_host+':'+ambari_server_port+'/api/v1/clusters', auth=('admin', 'admin')).content).get('items')[0].get('Clusters').get('cluster_name'))
 
 install_dir = config['configurations']['historian-config']['historian.install.dir']
+historian_repo_username = config['configurations']['historian-config']['historian.historian.repo.username']
+historian_repo_password = config['configurations']['historian-config']['historian.historian.repo.password']
 historian_repo = config['configurations']['historian-config']['historian.historian.repo']
+historian_repo = historian_repo.replace('https://','https://'+historian_repo_username+':'+historian_repo_password+'@')
 simulator_repo = config['configurations']['historian-config']['historian.simulator.repo']
 
 atlas_host_ip = socket.gethostbyname(str(json.loads(requests.get('http://'+ambari_server_host+':'+ambari_server_port+'/api/v1/clusters/'+cluster_name+'/services/ATLAS/components/ATLAS_SERVER', auth=('admin', 'admin')).content).get('host_components')[0].get('HostRoles').get('host_name')))
