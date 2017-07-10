ba04a6b411634ef8ee3d778ecad183125edd5cdd
vvaks <vvaks@hortonworks.com>
2017-07-10 13:59:25 -0600

    update

--- recipes/historian-service-install.sh
+++ recipes/historian-service-install.sh
@@ -203,7 +203,7 @@ getHostByPosition (){
 deployTemplateToNifi () {
        	echo "*********************************Importing NIFI Template..."
        	# Import NIFI Template HDF 3.x
-       	TEMPLATEID=$(curl -v -F template=@"$ROOT_PATH/CloudBreakArtifacts/hdf-config/nifi-template/orchestrator.xml" -X POST http://$NIFI_HOST:9090/nifi-api/process-groups/root/templates/upload | grep -Po '<id>([a-z0-9-]+)' | grep -Po '>([a-z0-9-]+)' | grep -Po '([a-z0-9-]+)')
+       	TEMPLATEID=$(curl -v -F template=@"$ROOT_PATH/historian/src/nifi/orchestrator.xml" -X POST http://$NIFI_HOST:9090/nifi-api/process-groups/root/templates/upload | grep -Po '<id>([a-z0-9-]+)' | grep -Po '>([a-z0-9-]+)' | grep -Po '([a-z0-9-]+)')
        		sleep 1
 
        		# Instantiate NIFI Template 3.x
