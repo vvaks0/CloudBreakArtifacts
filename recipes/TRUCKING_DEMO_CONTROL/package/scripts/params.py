db531d11fa32f566b4a99a591d0fb9834be231ef
vvaks <vvaks@hortonworks.com>
2017-08-30 17:45:46 -0400

    new trucking demo control param fields

--- recipes/TRUCKING_DEMO_CONTROL/package/scripts/params.py
+++ recipes/TRUCKING_DEMO_CONTROL/package/scripts/params.py
@@ -5,5 +5,6 @@ from resource_management import *
 # server configurations
 config = Script.get_config()
 
-install_dir = config['configurations']['control-config']['democontrol.install_dir']
-download_url = config['configurations']['control-config']['democontrol.download_url']
+install_dir = config['configurations']['control-config']['democontrol.install.dir']
+download_url = config['configurations']['control-config']['democontrol.download.url']
+sam_extentions_download_url = config['configurations']['control-config']['democontrol.sam.extensions.git.url']
