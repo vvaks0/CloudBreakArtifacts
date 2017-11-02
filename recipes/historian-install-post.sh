cd6ac14977539b449c045580f75dc09bbaa0eb7d
vvaks <vvaks@hortonworks.com>
2017-11-02 07:27:57 -0400

    upgrade HDF management pack to 3.0.1.1

--- recipes/historian-install-post.sh
+++ recipes/historian-install-post.sh
@@ -576,8 +576,8 @@ installDruidService () {
 }
 
 instalHDFManagementPack () {
-	wget http://public-repo-1.hortonworks.com/HDF/centos7/3.x/updates/3.0.0.0/tars/hdf_ambari_mp/hdf-ambari-mpack-3.0.0.0-453.tar.gz
-ambari-server install-mpack --mpack=hdf-ambari-mpack-3.0.0.0-453.tar.gz --verbose
+	wget http://public-repo-1.hortonworks.com/HDF/centos7/3.x/updates/3.0.1.1/tars/hdf_ambari_mp/hdf-ambari-mpack-3.0.1.1-5.tar.gz
+ambari-server install-mpack --mpack=hdf-ambari-mpack-3.0.1.1-5.tar.gz --verbose
 
 	sleep 2
 	ambari-server restart
