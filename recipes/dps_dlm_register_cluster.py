c2b061b26af4514f73e1911e21999a584080a256
vvaks <vvaks@192.168.1.64>
2018-07-07 19:37:59 -0400

    bug fix

--- recipes/dps_dlm_register_cluster.py
+++ recipes/dps_dlm_register_cluster.py
@@ -255,13 +255,13 @@ def dps_register_cluster():
 
 def get_dlm_cluster_details(target_cluster_name):
     dlm_clusters = json.loads(requests.get(url=dps_url+dlm_clusters_uri, cookies=cookie, headers=headers, verify=False).content)
-    print dlm_clusters
+    #print dlm_clusters
     for dlm_cluster in dlm_clusters['clusters']:
-        print dlm_cluster['name'] +' ? '+ target_cluster_name
+        #print dlm_cluster['name'] +' ? '+ target_cluster_name
         if dlm_cluster['name'] == target_cluster_name:
             return dlm_cluster
-        else:
-            return None
+    
+    return None
     
 def dlm_pair_clusters(source_cluster, destination_cluster):
     headers={'content-type':'application/json'}
