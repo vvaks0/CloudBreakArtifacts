41a46cdb61d970fee9982cfc2e9b67a5101c4f1e
vvaks <vvaks@192.168.1.64>
2018-07-08 12:24:05 -0400

    try catch KeyError check_external

--- recipes/dps_dlm_register_cluster.py
+++ recipes/dps_dlm_register_cluster.py
@@ -276,11 +276,12 @@ def dlm_create_policy(source_cluster, destination_cluster, dataset_name, replica
     print 'Result: ' + requests.post(url=dps_url+dlm_clusters_uri+'/'+str(destination_cluster['id'])+'/policy/'+replicationPolicyName+'/submit', cookies=cookie, data=payload, headers=headers, verify=False).content
 
 def check_external_argument(argument_name):
-    if get_latest_config(dps_host_config_file)[argument_name] == None or not get_latest_config(dps_host_config_file)[argument_name]:
-        print argument_name + ' is null, make sure the proprety has been configured in Ambari under ' + dps_host_config_file
-        return False
-    else:
+    try:
+        get_latest_config(dps_host_config_file)[argument_name]
         return True
+    except KeyError, e:
+        print argument_name + ' is null, make sure the property has been configured in Ambari under ' + dps_host_config_file
+        return False
 
 ##################################################
 
