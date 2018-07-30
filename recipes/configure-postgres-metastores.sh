d452fa9089e8e0b0d7356b7dee927aef4305829d
vvaks <vvaks@192.168.1.64>
2018-07-30 13:24:52 -0400

    add user ranger to pg_hba

--- recipes/configure-postgres-metastores.sh
+++ recipes/configure-postgres-metastores.sh
@@ -23,9 +23,9 @@ if [[ $(cat /etc/system-release|grep -Po Amazon) == "Amazon" ]]; then
 	echo '' >  /var/lib/pgsql/9.5/data/pg_hba.conf
 	echo 'host  ambari ambari 									    0.0.0.0/0 		md5			' >> /var/lib/pgsql/9.5/data/pg_hba.conf
 	echo 'local ambari ambari 									              		md5			' >> /var/lib/pgsql/9.5/data/pg_hba.conf
-	echo 'local all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid           trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
-	echo 'host  all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid 0.0.0.0/0 trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
-	echo 'host  all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid ::/0      trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
+	echo 'local all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid           trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
+	echo 'host  all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid 0.0.0.0/0 trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
+	echo 'host  all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid ::/0      trust		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
 	echo 'local all             all                                     				peer			' >> /var/lib/pgsql/9.5/data/pg_hba.conf
 	echo 'host  all             all             127.0.0.1/32            		 		ident		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
 	echo 'host  all             all             ::1/128                 		 		ident		' >> /var/lib/pgsql/9.5/data/pg_hba.conf
@@ -35,9 +35,9 @@ else
 	echo '' >  /var/lib/pgsql/data/pg_hba.conf
 	echo 'host  ambari ambari 									    0.0.0.0/0 		md5			' >> /var/lib/pgsql/data/pg_hba.conf
 	echo 'local ambari ambari 									              		md5			' >> /var/lib/pgsql/data/pg_hba.conf
-	echo 'local all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid           trust		' >> /var/lib/pgsql/data/pg_hba.conf
-	echo 'host  all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid 0.0.0.0/0 trust		' >> /var/lib/pgsql/data/pg_hba.conf
-	echo 'host  all postgres,hive,rangerdba,rangeradmin,rangerlogger,druid ::/0      trust		' >> /var/lib/pgsql/data/pg_hba.conf
+	echo 'local all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid           trust		' >> /var/lib/pgsql/data/pg_hba.conf
+	echo 'host  all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid 0.0.0.0/0 trust		' >> /var/lib/pgsql/data/pg_hba.conf
+	echo 'host  all postgres,hive,ranger,rangerdba,rangeradmin,rangerlogger,druid ::/0      trust		' >> /var/lib/pgsql/data/pg_hba.conf
 	echo 'local all             all                                     		 		peer			' >> /var/lib/pgsql/data/pg_hba.conf
 	echo 'host  all             all             127.0.0.1/32            		 		ident		' >> /var/lib/pgsql/data/pg_hba.conf
 	echo 'host  all             all             ::1/128                 		 		ident		' >> /var/lib/pgsql/data/pg_hba.conf
